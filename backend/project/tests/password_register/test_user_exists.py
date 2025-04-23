fe32ccd130bfff711f30fbc3edf0835965709625
Everaldo451 <everaldo.work.jr@gmail.com>
2025-04-23 17:04:17 -0300

    new register tests

--- backend/project/tests/password_register/test_user_exists.py
+++ backend/project/tests/password_register/test_user_exists.py
@@ -2,9 +2,12 @@ from rest_framework.response import Response
 import pytest
 
 @pytest.mark.django_db
-def test_company_bad_request(client, endpoint, csrf_token, user_data, company_data):
+def test_company_exists(client, endpoint, csrf_token, create_company, user_data, company_data):
+    user, company = create_company
+    assert user is not None and company is not None
     assert csrf_token is not None
-    company_data.pop("name")
+    user_data["email"] = "other@gmail.com"
+    company_data["is_company"] = "on"
     response:Response = client.post(endpoint,
         data={
             **user_data,
@@ -13,17 +16,17 @@ def test_company_bad_request(client, endpoint, csrf_token, user_data, company_da
         }
     )
 
-    assert response.status_code==400
+    assert response.status_code==401
     json = response.json()
     assert isinstance(json, dict)
     message = json.get("message")
-    assert message=="Invalid company credentials."
+    assert message=="Company already exists."
 
 
 @pytest.mark.django_db
-def test_user_bad_request(client, endpoint, csrf_token, user_data):
+def test_user_exists(client, endpoint, csrf_token, create_user, user_data):
+    assert create_user is not None
     assert csrf_token is not None
-    user_data.pop("full_name")
     response:Response = client.post(endpoint,
         data={
             **user_data,
@@ -31,8 +34,8 @@ def test_user_bad_request(client, endpoint, csrf_token, user_data):
         }
     )
 
-    assert response.status_code==400
+    assert response.status_code==401
     json = response.json()
     assert isinstance(json, dict)
     message = json.get("message")
-    assert message=="Invalid user credentials."
+    assert message=="User already exists."
