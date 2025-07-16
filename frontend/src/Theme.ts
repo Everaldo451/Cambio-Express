3a594177e4d8cf794c96553014cce5e941176277
Everaldo451 <everaldo.work.jr@gmail.com>
2025-07-16 17:51:19 -0300

    delete react frontend

--- frontend/src/Theme.ts
+++
@@ -1,8 +0,0 @@
-import { ThemeType } from "./Types"
-
-export const commonTheme:ThemeType = {
-    bgColor: "#3F4156",
-    fontFamily: "InstrumentSans",
-    boxShadowColor: "#2D2F3F",
-    divColor: "#BEC1C1",
-}
