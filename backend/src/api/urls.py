cd7ea2df70a9064aa3681c9eae85a386a0303cf5
Everaldo451 <everaldo.work.jr@gmail.com>
2025-07-17 22:20:32 -0300

    delete api urls

--- backend/src/api/urls.py
+++ backend/src/api/urls.py
@@ -1,15 +1,11 @@
 from django.urls import path, include
-from .views import graphs, swagger
+from .views import graphs
 
 urlpatterns = [
-    path("users/", include("users.urls")),
     path("feedbacks/",include("feedbacks.urls")),
-    path("accounts/", include("accounts.urls")),
-    path("me/", include("me.urls")),
     path("offerts/", include("offerts.urls")),
     path("auth/", include("authentication.urls")),
     path("graphs/",include([
         path("get/<coin>",graphs.get)
     ])),
-    path("swagger-ui/", swagger.schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui")
 ]
