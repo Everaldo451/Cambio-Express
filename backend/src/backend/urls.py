9a3b7ca3a2b618b25c5e5bc5d67beddb92f847be
Everaldo451 <everaldo.work.jr@gmail.com>
2025-07-17 22:21:55 -0300

    backend files

---
+++ backend/src/backend/urls.py
@@ -0,0 +1,50 @@
+"""
+URL configuration for project project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+
+from rest_framework import routers
+
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+from users import views as user_views
+from accounts import views as account_views
+
+from decouple import config
+
+router = routers.DefaultRouter()
+router.register(r'users', user_views.UserViewSet, basename='users')
+router.register(r'accounts', account_views.AccountViewSet, basename='accounts')
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Cambio Express API",
+        default_version="v1",
+    ),
+    public=False
+)
+
+urlpatterns = [
+    path(config("DJANGO_ADMIN_URL", 'admin/'), admin.site.urls),
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('api/v1/', include([
+    	path('', include(router.urls)),
+        path('auth/', include('authentication.urls')),
+    ])),
+    path("", include("api.urls"))
+]
