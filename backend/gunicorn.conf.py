6a7c461ab60543b3906e068666ad4cd68c646630
Everaldo451 <everaldo.work.jr@gmail.com>
2025-08-16 20:24:28 -0300

    fix gunicorn error

--- backend/gunicorn.conf.py
+++ backend/gunicorn.conf.py
@@ -1,8 +1,3 @@
 import multiprocessing
-from decouple import config
 
-HOST = config('HOST')
-PORT = config('PORT')
-
-bind = f"{HOST}:{PORT}"
 workers = multiprocessing.cpu_count() * 2 + 1
--- backend/run.sh
+++ backend/run.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 poetry run python src/manage.py migrate
 poetry run python src/manage.py collectstatic
-poetry run python src/manage.py runserver $HOST:$PORT
-#poetry run gunicorn src.backend.wsgi:application
+poetry run gunicorn -c gunicorn.conf.py src.backend.wsgi:application --bind $HOST:$PORT
